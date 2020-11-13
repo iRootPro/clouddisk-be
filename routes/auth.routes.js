d47fc225205a8561df76e5fe70d82a31fabd2023
Alexander Neupokoev <admin@armavir.ru>
2020-11-13 19:17:56 +0300

    feat: login, res json with token or error

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,12 @@
 const Router = require("express")
+const config = require("config")
 const bcrypt = require("bcryptjs")
+const jsonwebtoken = require("jsonwebtoken")
 const {check, validationResult} = require("express-validator")
 const User = require("./../models/User")
 const router = Router()
 
+const secretKey = config.get('secretKey')
 
 router.post('/registration',
     [
@@ -24,7 +27,7 @@ router.post('/registration',
                 return res.status(400).json({message: `User with ${email} already exist`})
             }
 
-            const hashPassword = await bcrypt.hash(password, 15)
+            const hashPassword = await bcrypt.hash(password, 8)
             const user = new User({email, password: hashPassword})
             await user.save()
             return res.status(200).json({message: 'User was created'})
@@ -33,5 +36,33 @@ router.post('/registration',
             return res.send({message: "Server error"})
         }
     })
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(404).json({message: 'User not found'})
+        }
+        const isPassValid = bcrypt.compareSync(password, user.password)
+        if (!isPassValid) {
+            return res.status(400).json({message: 'Invalid password'})
+        }
+        const token = jsonwebtoken.sign({id: user.id}, secretKey, {expiresIn: '1h'})
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                diskSpace: user.diskSpace,
+                usedSpace: user.usedSpace,
+                avatar: user.avatar
+            }
+        })
+    } catch (e) {
+        console.error(e)
+        return res.send({message: "Server error"})
+    }
+})
 
 module.exports = router
